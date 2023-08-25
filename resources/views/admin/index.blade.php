ea12043815197a4accf3a2d24a87d2649978e290
Juan Antonio Castañeda <jcastan1@hotmail.com>
2023-08-25 10:39:23 -0600

    creando parrafo desde master

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -11,4 +11,7 @@
     <p>Esto es un parrafo</p>
     <p>Welcome to this beautiful admin panel.</p>
     <p>Esto es un parrafo y aqui aparecera mas contenido en un futuro</p>
+
+
+    <p>Aqui hay otro cambio</p>
 @stop
